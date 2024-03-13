8f1a484aa5a7eea49ab9c61f1b6a41be86c3a73c
H31M44\Student <Student>
2024-03-13 19:00:35 +0700

    week

---
+++ src/main/java/vn/com/edu/fit/lab_week_05/LabWeek05Application.java
@@ -0,0 +1,13 @@
+package vn.com.edu.fit.lab_week_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWeek05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWeek05Application.class, args);
+    }
+
+}
